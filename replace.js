f24ccd46bd91d63277b3dda1a82397abca52d19b
PokotyloTaras <taras.h.pokotylo@ukd.edu.ua>
2025-11-17 22:02:20 +0200

    fixed problem that spaces were not changing

--- replace.js
+++ replace.js
@@ -1,11 +1,10 @@
-const fs = require('node:fs');
+const fs = require('fs');
 
   const data = fs.readFileSync(
     'paranesi.txt', 
     { 
         encoding:'utf8',
         flag: 'r'
-}
-);
-
-    console.log(data.replaceAll(/\n/, " ", "POKOTYLO TARASIK"))
+    }
+    );
+    console.log(data.replaceAll(" ", "POKOTYLO TARASIK"))
